12ad1c2e15ccce2869273438dc8aa4386a1b4cfd
midroid <ch.m.aakash@gmail.com>
2015-12-15 21:55:21 +0900

    Materialize, good to go

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Setting up global variable to use for css and js purposes
+global.appRoot = path.resolve(__dirname);
+
 app.use('/', routes);
 app.use('/users', users);
 
