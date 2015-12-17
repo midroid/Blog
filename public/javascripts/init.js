e74e083a590186d6aa1a9482ef4a98bf866fe13d
midroid <ch.m.aakash@gmail.com>
2015-12-18 00:26:58 +0900

    adding the modal for the login

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,8 +1,14 @@
 (function($){
   $(function(){
-
+    
+    //SideNav Bar initialization
     $('.button-collapse').sideNav();
+    
+    //Parallax effect initialization
     $('.parallax').parallax();
+    
+    //jQuery Modal Plugin
+    $('.modal-trigger').leanModal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
